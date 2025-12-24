6eb6474e545286d958ca3e8bcc5838148a14b8fc
Rajat Nagar <rajatnagar7893@gmail.com>
2025-12-24 14:37:20 +0530

    Add atatus or message for backend running

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from "@nestjs/common";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-// endpoint to get a hello message
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  health() {
+    return { status: "Hello Backend is running" };
   }
 }
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,12 @@ import { ProductsModule } from "./modules/products/products.module"
 import { OrdersModule } from "./modules/orders/orders.module"
 import { PrismaService } from "./prisma/prisma.service"
 import { PrismaModule } from "./prisma/prisma.module"
+import { AppController } from "./app.controller"
+import { AppService } from "./app.service"
 
 @Module({
   imports: [PrismaModule, ProductsModule, OrdersModule],
   providers: [PrismaService],
+  controllers: [AppController],
 })
 export class AppModule {}
